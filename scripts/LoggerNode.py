1e688c3a1f32b6cd51b5878de4649638a3c6cc5a
mikk366b <mikk366b@gmail.com>
2023-04-04 00:48:24 -0700

    New data

--- scripts/LoggerNode.py
+++ scripts/LoggerNode.py
@@ -106,7 +106,7 @@ def save_log():
 			#if (speed != 0.0):
 			#	desired_velocity = desired_velocity_yaml
 			
-			if (lastdatareading == None or lastdatareading != newdatareading or desired_velocity == 0.0):
+			if (lastdatareading == None or desired_velocity == 0.0): #or lastdatareading != newdatareading
 				file.write('%f, %f, %f, %f, %f, %f, %f\n' % (data_stream.pose.pose.position.x,
 												data_stream.pose.pose.position.y,
 												euler[2],
